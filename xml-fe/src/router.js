7474c81759a41c9b335bc9d7001c84e2b319e9bf
Branko Gluvajić <gluvajicbranko@gmail.com>
2020-06-12 17:58:20 +0200

    Axios Installed, Login Page 1.0

--- xml-fe/src/router.js
+++ xml-fe/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import LoginPage from '@/components/homepage/LoginPage'
 
 Vue.use(Router)
 
@@ -8,6 +9,9 @@ export default new Router({
     mode: 'history',
     
     routes: [
-        
+        {
+            path: '/login',
+            component: LoginPage
+        }
     ]
 })
