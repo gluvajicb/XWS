e96db1a6e2e38bfd5c1023e51524a4346394be09
Vukasin Petrovic <vule97@gmail.com>
2020-06-18 22:56:08 +0200

    - Ispravljen cors

--- xml-be/be/src/main/java/xmlproject/be/BeApplication.java
+++ xml-be/be/src/main/java/xmlproject/be/BeApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BeApplication {
@@ -11,6 +13,19 @@ public class BeApplication {
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
+						.allowedOrigins("*")
+						.allowedHeaders("*");
+			}
+		};
 	}
 
 	public static void main(String[] args) {
