7d12a4b144db80056ed341e2390e509c819fbff9
unknown <nikolinaapetrovic97@gmail.com>
2020-06-19 16:12:50 +0200

    My articles added

--- frontend/src/app/reviewed-articles/reviewed-articles.component.ts
+++ frontend/src/app/reviewed-articles/reviewed-articles.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ArticleService } from '../services/article.service';
 
 @Component({
   selector: 'app-reviewed-articles',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewedArticlesComponent implements OnInit {
 
-  constructor() { }
 
+  constructor(private ArticleService: ArticleService) { }
+  articles: [];
+  status: string;
   ngOnInit(): void {
+    this.status = "reviewing";
+    this.articles = [];
+    this.getArticles();
   }
 
+  getArticles() {
+    var searchParams_JSON =
+                     {
+                         "abst": "" ,
+                         "title": "",
+                         "keyword": "",
+                         "author": "",
+                         "section": "",
+                         "status": this.status
+                     };
+
+    console.log(searchParams_JSON)
+
+    this.ArticleService.searchArticle(searchParams_JSON).subscribe(
+      result => {
+        this.articles = result
+        console.log(result)
+      }
+    );
+  }
 }
