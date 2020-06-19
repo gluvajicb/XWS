b1870d4436977b00a03b112537b16be96e170633
Branko Gluvajić <gluvajicbranko@gmail.com>
2020-06-19 16:01:40 +0200

    My Articles Component

--- frontend/src/app/my-articles/my-articles.component.ts
+++ frontend/src/app/my-articles/my-articles.component.ts
@@ -13,7 +13,9 @@ export class MyArticlesComponent implements OnInit {
 
   constructor(private ArticleService: ArticleService) { }
   status: String;
-  articles: [];
+  articles: JSON[];
+
+
   ngOnInit(): void {
     this.articles = [];
     this.status = "accepted";
@@ -42,7 +44,46 @@ export class MyArticlesComponent implements OnInit {
     this.ArticleService.searchArticle(searchParams_JSON).subscribe(
       result => {
         this.articles = result
+        console.log("Rezultat ispod")
         console.log(result)
+
+        /* ... */
+        let parseString = require('xml2js').parseString;
+        let i;
+        this.articles.splice(0, this.articles.length)
+        for (i = 0; i < result.length; i++) {
+          console.log(result[i]);
+          let res;
+          let authors = [];
+          let keywords = [];
+          
+          parseString(result[i], function (err, result1) {
+            
+            console.log(result1)
+
+            let j, k;
+            let authors = []
+            for(j = 0; j < result1["ns1:article"]["ns1:authors"][0]["ns1:author"].length; j ++){
+
+              authors[j] = result1["ns1:article"]["ns1:authors"][0]["ns1:author"][j]["ns1:name"] + " " + result1["ns1:article"]["ns1:authors"][0]["ns1:author"][j]["ns1:surname"];
+            }
+            if(result1["ns1:article"]["ns1:abstract"][0]["ns1:keywords"] !== undefined) {
+              for(k = 0; k < result1["ns1:article"]["ns1:abstract"][0]["ns1:keywords"].length; k++){
+
+                keywords[k] = result1["ns1:article"]["ns1:abstract"][0]["ns1:keywords"][k]["ns1:keyword"];
+              }
+            }
+            
+            let retVal = {
+              "title": result1["ns1:article"]["ns1:title"],
+              "authors": authors,
+              "keywords" : keywords
+            }
+            res = retVal
+          });
+
+          this.articles[i] = res
+        }
       }
     );
   }
